48b8da12f85f9f6a5a729e0f16826e5bc0093d83
Arnisfet <mrudge@student.21-school.ru>
2022-08-01 11:13:28 -0400

    It's all!

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -4,6 +4,7 @@ if [ -f ./wordpress/wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
+	#Paste string for listen 9000 in php-fpm
 	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
 	chmod -R 775 /var/www/html/wordpress;
 	chown -R www-data /var/www/html/wordpress;
@@ -17,14 +18,21 @@ else
 	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
 	mv ./www.conf /etc/php/7.3/fpm/pool.d/
 
-	#Inport env variables in the config file
-	#cd /var/www/html/wordpress
-	#sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	#sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	#sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	#sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	#mv wp-config-sample.php wp-config.php
 	cp /var/www/html/wp-config.php /var/www/html/wordpress/wp-config.php 
+	
+	cd /tmp/
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	chmod +x wp-cli.phar;
+	mv wp-cli.phar /usr/local/bin/wp;
+	cd /var/www/html/wordpress/
+	
+	wp core install --allow-root --url=${DOMAIN_NAME} --title=${WORDPRESS_NAME} --admin_user=${WORDPRESS_ROOT_LOGIN} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=${WORDPRESS_ROOT_EMAIL};
+	wp user create ${MYSQL_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MYSQL_PASSWORD} --role=author --allow-root;
+	wp theme install twentyseventeen --activate --allow-root;
+	
+
+
 fi
 
+/usr/sbin/php-fpm7.3 -F;
 exec "$@"
